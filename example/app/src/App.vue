dc964b9b02a18c747fe324ff08cde770ce6d7448
SteveM <SteveM@myEtherWallet.com>
2020-06-30 14:56:48 -0700

    publish v2.1.0-RC.5.6

--- example/app/src/App.vue
+++ example/app/src/App.vue
@@ -156,7 +156,6 @@ export default {
     this.web3 = new Web3(this.ethereum);
     // See the 'onClick' method below for starting the connection sequence
     // listener on the web3 provider emiting when the account changes (at the moment this is also the same as a connection being established.)
-    console.log( this.ethereum.on); // todo remove dev item
     this.ethereum.on('accountsChanged', accounts => {
       console.log(`accountsChanged User's address is ${accounts[0]}`);
     });
