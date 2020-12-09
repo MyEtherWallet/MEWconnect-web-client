5b998a83246a1d820baf3290736e3df5216797eb
Victor Porton <porton@narod.ru>
2020-12-09 16:33:11 +0200

    bug fix

--- src/connectProvider/web3Provider/networks/types/index.js
+++ src/connectProvider/web3Provider/networks/types/index.js
@@ -2,5 +2,7 @@ import ETH from './ETH.js';
 import GOERLI from './GOERLI.js';
 import KOV from './KOV.js';
 import ROP from './ROP';
+import MATIC from './MATIC';
+import MUMBAI from './MUMBAI';
 
-export { ETH, GOERLI, KOV, ROP };
+export { ETH, GOERLI, KOV, ROP, MATIC, MUMBAI };
