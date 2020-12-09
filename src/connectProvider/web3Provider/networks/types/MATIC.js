9266c77104a2567137778ecc39727a82571c50fb
Victor Porton <porton@narod.ru>
2020-12-09 16:06:56 +0200

    Matic network support

---
+++ src/connectProvider/web3Provider/networks/types/MATIC.js
@@ -0,0 +1,13 @@
+import tokens from '../tokens/tokens-eth.json';
+
+export default {
+  name: 'MATIC',
+  name_long: 'Matic',
+  homePage: 'https://matic.network',
+  blockExplorerTX: 'https://explorer-mainnet.maticvigil.com/tx/[[txHash]]',
+  blockExplorerAddr: 'https://explorer-mainnet.maticvigil.com/address/[[address]]',
+  chainID: 137,
+  tokens: [],
+  contracts: [],
+  currencyName: 'MATIC'
+};
