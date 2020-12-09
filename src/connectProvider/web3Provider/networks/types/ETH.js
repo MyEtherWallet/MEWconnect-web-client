5b29ec6d3574a4bc54cae3bbea766ae0b6e51698
Victor Porton <porton@narod.ru>
2020-12-09 16:25:39 +0200

    restored lost ETH.js file

---
+++ src/connectProvider/web3Provider/networks/types/ETH.js
@@ -0,0 +1,13 @@
+import tokens from '../tokens/tokens-eth.json';
+
+export default {
+  name: 'ETH',
+  name_long: 'Ethereum',
+  homePage: 'https://ethereum.org',
+  blockExplorerTX: 'https://etherscan.io/tx/[[txHash]]',
+  blockExplorerAddr: 'https://etherscan.io/address/[[address]]',
+  chainID: 1,
+  tokens: tokens,
+  contracts: [],
+  currencyName: 'ETH'
+};
