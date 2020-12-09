6ab0e107ce305cb744f6cb683acf2cbaf8b054a7
Victor Porton <porton@narod.ru>
2020-12-09 16:08:21 +0200

    Mumbai testnet support

---
+++ src/connectProvider/web3Provider/networks/types/MUMBAI.js
@@ -0,0 +1,13 @@
+import tokens from '../tokens/tokens-eth.json';
+
+export default {
+  name: 'MUMBAI',
+  name_long: 'Mumbai',
+  homePage: 'https://matic.network',
+  blockExplorerTX: 'https://mumbai-explorer.matic.today/tx/[[txHash]]',
+  blockExplorerAddr: 'https://mumbai-explorer.matic.today/address/[[address]]',
+  chainID: 80001,
+  tokens: [],
+  contracts: [],
+  currencyName: 'MATIC'
+};
