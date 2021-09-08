2077238da40fe01bcfa4024b70b20b55c0044e4d
Kosala Hemachandra <10602065+kvhnuke@users.noreply.github.com>
2021-09-08 13:52:19 -0700

    test signTypedData_v4

--- src/clientExample/clientExample.vue
+++ src/clientExample/clientExample.vue
@@ -122,6 +122,12 @@
       </li>
       <li>
         <hr />
+        <button @click="signTypedDataV4">
+          signed Typed v4
+        </button>
+      </li>
+      <li>
+        <hr />
         <input v-model="messageToSign" />
         <button @click="signMessage">sign message</button><br />
         <textarea
@@ -395,6 +401,64 @@ export default {
           });
         });
       });
+    },
+    signTypedDataV4() {
+      const data = {
+        types: {
+          EIP712Domain: [
+            { name: 'name', type: 'string' },
+            { name: 'version', type: 'string' },
+            { name: 'chainId', type: 'uint256' },
+            { name: 'verifyingContract', type: 'address' }
+          ],
+          Person: [
+            { name: 'name', type: 'string' },
+            { name: 'wallet', type: 'address' }
+          ],
+          Mail: [
+            { name: 'from', type: 'Person' },
+            { name: 'to', type: 'Person' },
+            { name: 'contents', type: 'string' }
+          ]
+        },
+        primaryType: 'Mail',
+        domain: {
+          name: 'Ether Mail',
+          version: '1',
+          chainId: 1,
+          verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC'
+        },
+        message: {
+          sender: {
+            name: 'Cow',
+            wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826'
+          },
+          recipient: {
+            name: 'Bob',
+            wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB'
+          },
+          contents: 'Hello, Bob!'
+        }
+      };
+      this.ethereum
+        .request({
+          method: 'eth_signTypedData_v4',
+          params: [this.userAddress, JSON.stringify(data)]
+        })
+        .then(sig => {
+          console.log('typed data sig', sig);
+          console.log(
+            sigUtil
+              .recoverTypedSignature(
+                {
+                  sig,
+                  data
+                },
+                'V4'
+              )
+              .toString('hex')
+          );
+        });
     },
     signTypedDataV3() {
       const data = {
